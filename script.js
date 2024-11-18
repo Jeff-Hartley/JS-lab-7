496a57cf6bae3f39779ff994f58a1857e28bebb6
Jeff <jeffhartley16@gmail.com>
2024-11-18 16:32:09 -0500

    Added functionality to keyboard events section

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Declaring variables with elements by id.
+//For Mouse Event
 const mouseEventBtn = document.getElementById('mouseEventBtn');
 const mouseEventMsg = document.getElementById('mouseEventMsg');
+
+//For Keyboard Event
+const keyboardEventInput = document.getElementById('keyboardEventInput');
+const keyboardEventMsg = document.getElementById('keyboardEventMsg');
 
 //event listener for hovering over the button
 mouseEventBtn.addEventListener("mouseenter", (event) =>{
@@ -10,4 +15,8 @@ mouseEventBtn.addEventListener("mouseenter", (event) =>{
 //event listener for when you stop hovering over the button
 mouseEventBtn.addEventListener("mouseleave", (event) => {
     mouseEventMsg.textContent = "Hover over the button to reveal the secret message!" // message that will appear when you stop hovering
+});
+
+keyboardEventInput.addEventListener("keydown", (event) => {
+    keyboardEventMsg.textContent = event.key;
 });
